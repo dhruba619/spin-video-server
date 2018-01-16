cc178279f346156f424ac22a3af1d8b492041ff3
Piyali <piyali.ria@gmail.com>
2018-01-16 21:35:22 +0530

    added pom and src

---
+++ src/main/java/org/spinstatz/videoserver/VideoServerApplication.java
@@ -0,0 +1,12 @@
+package org.spinstatz.videoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoServerApplication.class, args);
+	}
+}
